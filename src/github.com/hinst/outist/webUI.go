a6fd8536d75843c09f28a7626d0094c7f130de28
hinst <hinstance@yandex.ru>
2016-06-20 14:46:49 +0300

    page

--- src/github.com/hinst/outist/webUI.go
+++ src/github.com/hinst/outist/webUI.go
@@ -6,24 +6,28 @@ import (
 	"net/http"
 )
 
-const FileDirectory = "src/github.com/hinst/outist_page"
+const PageDirectory = "src/github.com/hinst/outist_page"
 
 type TWebUI struct {
-	URL string
+	URL     string
+	Pages   []string
+	UserMan *TUserMan
 }
 
 func CreateWebUI() *TWebUI {
 	var result = &TWebUI{}
+	result.Pages = []string{"login"}
 	return result
 }
 
 func (this *TWebUI) Prepare() {
 	this.registerFile("concise.css")
+	http.HandleFunc(this.URL+"/page", this.ProcessPageRequest)
 }
 
 func (this *TWebUI) registerFile(file string) {
 	var url = this.URL + "/" + file
-	var filePath = AppDirectory + "/" + FileDirectory + "/" + file
+	var filePath = AppDirectory + "/" + PageDirectory + "/" + file
 	GlobalLog.Write(url + " -> " + filePath)
 	http.HandleFunc(url,
 		func(response http.ResponseWriter, request *http.Request) {
@@ -34,7 +38,7 @@ func (this *TWebUI) registerFile(file string) {
 func (this *TWebUI) loadPage(data TPageData) string {
 	var result = ""
 	data.Body = this.substituteBody(data)
-	var lTemplate, parseResult = template.ParseFiles(AppDirectory + "/" + FileDirectory + "/template.html")
+	var lTemplate, parseResult = template.ParseFiles(AppDirectory + "/" + PageDirectory + "/template.html")
 	if parseResult == nil {
 		var output bytes.Buffer
 		var executeResult = lTemplate.Execute(&output, data)
@@ -64,4 +68,20 @@ func (this *TWebUI) substituteBody(data TPageData) string {
 		GlobalLog.Write("Error: could not parse template: " + templateParseResult.Error())
 	}
 	return result
+}
+
+func (this *TWebUI) CreatePageData() TPageData {
+	var result TPageData
+	result.AppURL = this.URL
+	return result
+}
+
+func (this *TWebUI) ProcessPageRequest(response http.ResponseWriter, request *http.Request) {
+	var pageName = request.URL.Query().Get("page")
+	if CheckStringArrayContains(this.Pages, pageName) {
+		var page = this.CreatePageData()
+		page.Body = ReadStringFromFile(AppDirectory + "/" + PageDirectory + "/" + pageName + ".html")
+		var content = this.loadPage(page)
+		response.Write([]byte(content))
+	}
 }
